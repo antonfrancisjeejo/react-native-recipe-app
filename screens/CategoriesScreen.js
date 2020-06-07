fcb4cd58c1ace958eb1d245f29afdd52a783543a
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-06-08 00:00:25 +0530

    Initial commit

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+
+const CategoriesScreen = ({ navigation }) => {
+  navigation.setOptions({
+    headerTitle: "Meal Categories",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => navigation.toggleDrawer()}
+        />
+      </HeaderButtons>
+    ),
+  });
+  const renderGridItem = (itemData) => {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          navigation.navigate("CategoryMeals", {
+            categoryId: itemData.item.id,
+          });
+        }}
+      />
+    );
+  };
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      numColumns={2}
+      renderItem={renderGridItem}
+    />
+  );
+};
+
+export default CategoriesScreen;
