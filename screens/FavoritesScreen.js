a9f41094016c410f781c69cdb146937275ceb479
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-03-04 23:07:05 +0530

    Updated

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,27 +1,31 @@
-import React from "react";
-import MealList from "../components/MealList";
-import { useSelector } from "react-redux";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import HeaderButton from "../components/HeaderButton";
-import { View, StyleSheet } from "react-native";
-import DefaultText from "../components/DefaultText";
+import React, {useEffect} from 'react';
+import MealList from '../components/MealList';
+import {useSelector} from 'react-redux';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
+import {View, StyleSheet} from 'react-native';
+import DefaultText from '../components/DefaultText';
 
-const FavoritesScreen = ({ navigation }) => {
-  const favoriteMeals = useSelector((state) => {
+const FavoritesScreen = ({navigation}) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Your Favorites',
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => navigation.toggleDrawer()}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, []);
+
+  const favoriteMeals = useSelector(state => {
     return state.meals.favoriteMeals;
   });
-  navigation.setOptions({
-    headerTitle: "Your Favorites",
-    headerLeft: () => (
-      <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          title="Menu"
-          iconName="ios-menu"
-          onPress={() => navigation.toggleDrawer()}
-        />
-      </HeaderButtons>
-    ),
-  });
+
   if (favoriteMeals.length === 0 || !favoriteMeals) {
     return (
       <View style={styles.content}>
@@ -34,8 +38,8 @@ const FavoritesScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
