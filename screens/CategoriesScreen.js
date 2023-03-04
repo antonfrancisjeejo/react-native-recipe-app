2340253098a082a560ece368cf723ab4aeb6f0b1
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-03-04 23:06:57 +0530

    Updated

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,31 +1,34 @@
-import React from "react";
-import { FlatList } from "react-native";
+import React, {useEffect} from 'react';
+import {FlatList} from 'react-native';
 
-import { CATEGORIES } from "../data/dummy-data";
-import CategoryGridTile from "../components/CategoryGridTile";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import HeaderButton from "../components/HeaderButton";
+import {CATEGORIES} from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 
-const CategoriesScreen = ({ navigation }) => {
-  navigation.setOptions({
-    headerTitle: "Meal Categories",
-    headerLeft: () => (
-      <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          title="Menu"
-          iconName="ios-menu"
-          onPress={() => navigation.toggleDrawer()}
-        />
-      </HeaderButtons>
-    ),
-  });
-  const renderGridItem = (itemData) => {
+const CategoriesScreen = ({navigation}) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Meal Categories',
+      headerLeft: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => navigation.toggleDrawer()}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
+  const renderGridItem = itemData => {
     return (
       <CategoryGridTile
         title={itemData.item.title}
         color={itemData.item.color}
         onSelect={() => {
-          navigation.navigate("CategoryMeals", {
+          navigation.navigate('CategoryMeals', {
             categoryId: itemData.item.id,
           });
         }}
