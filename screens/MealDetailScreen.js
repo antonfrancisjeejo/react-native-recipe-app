f221b975babd4d76898a5114707b7457310742ff
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-03-04 23:07:16 +0530

    Updated

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { useSelector, useDispatch } from "react-redux";
-import HeaderButton from "../components/HeaderButton";
-import DefaultText from "../components/DefaultText";
-import { toggleFavorite } from "../store/actions/meals";
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Image, ScrollView} from 'react-native';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import {useSelector, useDispatch} from 'react-redux';
+import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
+import {toggleFavorite} from '../slices/meals';
 
-const ListItem = ({ children }) => {
+const ListItem = ({children}) => {
   return (
     <View style={styles.listItem}>
       <DefaultText>{children}</DefaultText>
@@ -14,17 +14,17 @@ const ListItem = ({ children }) => {
   );
 };
 
-const MealDetailScreen = ({ route, navigation }) => {
-  const { mealId } = route.params;
+const MealDetailScreen = ({route, navigation}) => {
+  const {mealId} = route.params;
 
-  const isFavoriteMeal = useSelector((state) => {
-    return state.meals.favoriteMeals.some((meal) => meal.id === mealId);
+  const isFavoriteMeal = useSelector(state => {
+    return state.meals.favoriteMeals.some(meal => meal.id === mealId);
   });
-  const availableMeals = useSelector((state) => {
+  const availableMeals = useSelector(state => {
     return state.meals.meals;
   });
 
-  const selectedMeal = availableMeals.find((meal) => {
+  const selectedMeal = availableMeals.find(meal => {
     return meal.id === mealId;
   });
 
@@ -34,40 +34,38 @@ const MealDetailScreen = ({ route, navigation }) => {
     dispatch(toggleFavorite(mealId));
   };
 
-  navigation.setOptions({
-    headerTitle: selectedMeal.title,
-    headerRight: () => (
-      <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          title="Favourite"
-          iconName={isFavoriteMeal ? "ios-star" : "ios-star-outline"}
-          onPress={toggleFavHandler}
-        />
-      </HeaderButtons>
-    ),
-  });
-  const {
-    duration,
-    affordability,
-    complexity,
-    imageUrl,
-    ingredients,
-    steps,
-  } = selectedMeal;
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: selectedMeal.title,
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Favourite"
+            iconName={isFavoriteMeal ? 'star' : 'star-outline'}
+            onPress={toggleFavHandler}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [isFavoriteMeal]);
+
+  const {duration, affordability, complexity, imageUrl, ingredients, steps} =
+    selectedMeal;
+
   return (
     <ScrollView>
-      <Image source={{ uri: imageUrl }} style={styles.image} />
+      <Image source={{uri: imageUrl}} style={styles.image} />
       <View style={styles.details}>
         <DefaultText>{duration}mins</DefaultText>
         <DefaultText>{complexity.toUpperCase()}</DefaultText>
         <DefaultText>{affordability.toUpperCase()}</DefaultText>
       </View>
       <Text style={styles.title}>Ingredients</Text>
-      {ingredients.map((ingredient) => {
+      {ingredients.map(ingredient => {
         return <ListItem key={ingredient}>{ingredient}</ListItem>;
       })}
       <Text style={styles.title}>Steps</Text>
-      {steps.map((step) => {
+      {steps.map(step => {
         return <ListItem key={step}>{step}</ListItem>;
       })}
     </ScrollView>
@@ -75,23 +73,23 @@ const MealDetailScreen = ({ route, navigation }) => {
 };
 const styles = StyleSheet.create({
   image: {
-    width: "100%",
+    width: '100%',
     height: 200,
   },
   details: {
-    flexDirection: "row",
+    flexDirection: 'row',
     padding: 15,
-    justifyContent: "space-around",
+    justifyContent: 'space-around',
   },
   title: {
-    fontFamily: "open-sans-bold",
+    fontFamily: 'open-sans-bold',
     fontSize: 22,
-    textAlign: "center",
+    textAlign: 'center',
   },
   listItem: {
     marginVertical: 10,
     marginHorizontal: 20,
-    borderColor: "#ccc",
+    borderColor: '#ccc',
     borderWidth: 1,
     padding: 10,
   },
