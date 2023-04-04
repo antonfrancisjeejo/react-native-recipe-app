6848cf70837b430840aff0d44a2c8e48012d5eb7
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-04-04 21:58:13 +0530

    Updated

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,15 @@
-import React, {useEffect} from 'react';
-import {FlatList} from 'react-native';
+import React, { useEffect } from "react";
+import { FlatList } from "react-native";
 
-import {CATEGORIES} from '../data/dummy-data';
-import CategoryGridTile from '../components/CategoryGridTile';
-import {HeaderButtons, Item} from 'react-navigation-header-buttons';
-import HeaderButton from '../components/HeaderButton';
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
-const CategoriesScreen = ({navigation}) => {
+const CategoriesScreen = ({ navigation }) => {
   useEffect(() => {
     navigation.setOptions({
-      headerTitle: 'Meal Categories',
+      headerTitle: "Meal Categories",
       headerLeft: () => (
         <HeaderButtons HeaderButtonComponent={HeaderButton}>
           <Item
@@ -22,13 +22,13 @@ const CategoriesScreen = ({navigation}) => {
     });
   }, [navigation]);
 
-  const renderGridItem = itemData => {
+  const renderGridItem = (itemData) => {
     return (
       <CategoryGridTile
         title={itemData.item.title}
         color={itemData.item.color}
         onSelect={() => {
-          navigation.navigate('CategoryMeals', {
+          navigation.navigate("CategoryMeals", {
             categoryId: itemData.item.id,
           });
         }}
